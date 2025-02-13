1526ebebbdb327b6c58e8a1711da4fd11b5766fb
doniyor-egamberdiyev-230300 <161677032+doniyor-egamberdiyev-230300@users.noreply.github.com>
2025-02-13 23:16:15 +0500

    Update main.py
    bgs

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@
 #ID:230300
 
 from task1 import kwargsAcceptFun
-kwargsAcceptFun(name="Alice", age=25, city="New York")
+
 
 from task2 import typeBasedTransformer
-print(typeBasedTransformer(a=5, b=3.5, c="Hello", d=True, e=[1, 2, 3], f={"x": 1, "y": 2}))
+
 
 from task3 import decorator_1
